591e618492c2dc0916a8fe22652ce7c84d110a41
Mourad BAHLOUL <mourad.bahloul@acensi.fr>
2023-05-23 10:50:05 +0200

    prise en compte des codes de retour des restTemplates et corrections au niveau du feature Cutdown

--- src/test/java/com/hc/train/cucumber/cucumberglue/CucumberMySteps.java
+++ src/test/java/com/hc/train/cucumber/cucumberglue/CucumberMySteps.java
@@ -5,6 +5,7 @@ import io.cucumber.java.en.When;
 import org.junit.jupiter.api.Assertions;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
@@ -16,22 +17,30 @@ public class CucumberMySteps {
     @LocalServerPort
     String port;
 
+    RestTemplate restTemplate = new RestTemplate();
+
     ResponseEntity<String> lastResponse;
 
     @When("the client calls endpoint {string}")
     public void whenClientCalls(String url) {
         try {
-            lastResponse = new RestTemplate().exchange("http://localhost:" + port + url, HttpMethod.GET, null,
+            lastResponse = restTemplate.exchange("http://localhost:" + port + url, HttpMethod.GET, null,
                     String.class);
         } catch (HttpClientErrorException httpClientErrorException) {
-            httpClientErrorException.printStackTrace();
+            //httpClientErrorException.printStackTrace();
+            HttpStatus statusCode = httpClientErrorException.getStatusCode();
+            String responseBody = httpClientErrorException.getResponseBodyAsString();
+            // You can also extract other relevant information like headers, etc.
+
+            // Create the ResponseEntity
+            lastResponse = new ResponseEntity<>(responseBody, statusCode);
         }
     }
 
     @Then("response status code is {int}")
     public void thenStatusCodee(int expected) {
-       // Assertions.assertNotNull(lastResponse);
-       // Assertions.assertNotNull(lastResponse.getStatusCode());
+       Assertions.assertNotNull(lastResponse);
+       Assertions.assertNotNull(lastResponse.getStatusCode());
         assertThat("status code is" + expected,
                 lastResponse.getStatusCodeValue() == expected);
     }
@@ -39,7 +48,7 @@ public class CucumberMySteps {
     @Then("returned string should be {string}")
     public void thenStringIs(String expected) {
         Assertions.assertEquals(expected, lastResponse.getBody());
-//        assertThat("Returned string is " + expected,
-//                expected.equalsIgnoreCase(lastResponse.getBody()));
+        assertThat("Returned string is " + expected,
+                expected.equalsIgnoreCase(lastResponse.getBody()));
     }
 }
